126fe3239e7fc4cb5aa1b5ac7220135d6fb2c412
Akshad Thole <55445013+akshadthole@users.noreply.github.com>
2022-09-29 04:31:04 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,11 +5,12 @@ class Main {
     System.out.println("Enter two numbers");
     int first = 10;
     int second = 20;
+    int third=50;
     
-    System.out.println(first + " " + second);
+    System.out.println(first + " " + second +" " +third);
 
     // add two numbers
-    int sum = first + second;
+    int sum = first + second+third;
     System.out.println("The sum is: " + sum);
   }
 }
